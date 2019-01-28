f0d153231687626fc828cf44c2b5287eacff5f54
SebastianoOlivo <olivo.sebastiano@gmail.com>
2019-01-28 11:20:25 +0100

    Empty the store

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,9 +1,1 @@
-import {createStore} from 'redux';
-import combineReducers from '../Reducers/index'
-import jokeReducers from '../Reducers/index'
 
-
-export const store = createStore(
-    combineReducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
